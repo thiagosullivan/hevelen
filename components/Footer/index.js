43c3c093fa45b97871a88e18c6414a431285e89e
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-09 07:21:11 -0300

    Footer section

---
+++ components/Footer/index.js
@@ -0,0 +1,89 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+import { GrFormCheckmark } from 'react-icons/gr';
+import { MdLocationPin, MdOutlineEmail, MdPhone } from 'react-icons/md';
+import FooterLogo from '../../assets/hevelen-logo.webp';
+import { FooterContainer } from './style';
+
+function Footer() {
+  return (
+    <FooterContainer>
+        <div className='footer__content'>
+            <div className='footer__top'>
+                <div className='footer__col1'>
+                    <Link href="/">
+                        <Image src={FooterLogo} width={200} height={150}/>
+                    </Link>
+                    <p>Far from the countries Vokalia and Consonantia, the live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+                </div>
+                <nav>
+                    <p>Links Rápidos</p>
+                    <ul>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">About Us</Link>
+                        </li>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">Testimonials</Link>
+                        </li>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">Classes</Link>
+                        </li>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">Blog</Link>
+                        </li>
+                        <li>
+                            <GrFormCheckmark />
+                            <Link href="/">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='footer__col3'>
+                    <p>Informações de Contato</p>
+                    <div className='contact__footer'>
+                        <div>
+                            <MdLocationPin />
+                            <p>Informações de Contato</p>
+                        </div>
+                        <a
+                            href='https://www.google.com.br'
+                            rel="noreferrer noopener"
+                            target="_blank"
+                        >
+                            <MdPhone />
+                            <p>+ 1235 2355 98</p>
+                        </a>
+                        <a
+                            href='https://www.google.com.br'
+                            rel="noreferrer noopener"
+                            target="_blank"
+                        >   
+                            <MdOutlineEmail />
+                            <p>info@yoursite.com</p>
+                        </a>
+                        <div>
+                            <AiOutlineClockCircle />
+                            <p>8:00 am - 6:00pm</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer__bottom'>
+                <p>© 2022 <strong>Hevelen Jeronymo</strong> - Desenvolvido por <strong>Digital Cloud Marketing</strong>.</p>
+            </div>
+        </div>
+
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ components/Footer/style.js
@@ -0,0 +1,121 @@
+import { lighten } from "polished";
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    background-color: ${({theme}) => theme.primary};
+    padding: 0 1rem;
+
+    .footer__content {
+        .footer__top {
+            max-width: 1400px;
+            width: 100%;
+            margin: 0 auto;
+            padding: 6.25rem 0;
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+
+            .footer__col1 {
+                max-width: 525px;
+                width: 100%;
+                height: auto;
+                display: flex;
+                
+                span {
+                    cursor: pointer;
+                }
+                p {
+                    max-width: 290px;
+                    color: ${({theme}) => theme.white};
+                    font-family: 'Montserrat', sans-serif;
+                    font-weight: 300;
+                    line-height: 25px;
+                    margin-left: 2rem;
+                }
+            }
+            nav {
+                p {
+                    font-family: 'Teko', sans-serif;
+                    color: ${({theme}) => theme.white};
+                    font-size: 2rem;
+                    margin-bottom: 1.5rem;
+                }
+                li {
+                    font-family: 'Montserrat', sans-serif;
+                    font-weight: 300;
+                    color: ${({theme}) => theme.white};
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: .1rem;
+
+                    svg {
+                        font-size: 2.5rem;
+                        margin-right: .2rem;
+
+                        polyline {
+                            stroke: ${({theme}) => theme.secondary};                            
+                            transition: all 150ms ease-out;
+                        }
+                    }
+                    a {
+                        font-size: 1rem;
+                        border: 1px solid transparent;
+                        transition: all 150ms ease-out;
+                    }
+
+                    &:hover a {
+                        border-bottom: 1px solid ${({ theme }) => theme.secondary};
+                    }
+                    &:hover polyline {
+                        stroke: ${({ theme }) => theme.white};
+                    }
+                }
+            }
+            .footer__col3 {
+                p:first-child {
+                    font-family: 'Teko', sans-serif;
+                    color: ${({theme}) => theme.white};
+                    font-size: 2rem;
+                    margin-bottom: 1.5rem;
+                }
+                .contact__footer {
+                    svg {
+                        color: ${({theme}) => theme.secondary};
+                        font-size: 2.5rem;
+                        margin-right: .8rem;
+                    }
+                    div {
+                        display: flex;
+                        align-items: center;
+                        margin-bottom: 1rem;
+
+                        p {
+                            color: ${({theme}) => theme.white};
+                        }
+                    }
+                    a {
+                        display: flex;
+                        align-items: center;
+                        color: ${({theme}) => theme.white};
+                        margin-bottom: 1rem;
+                        transition: all 150ms ease-out;
+
+                        &:hover {
+                            color: ${({theme}) => theme.secondary};
+                        }
+                        &:hover svg {
+                            color: ${({theme}) => theme.white};
+                        }
+                    }
+                }
+
+            }
+
+        }
+        .footer__bottom {
+            color: ${({theme}) => theme.white};
+            text-align: center;
+            padding-bottom: 3.75rem;
+        }
+    }
+`;
