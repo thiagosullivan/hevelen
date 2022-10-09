585eb18c1b050edda7aa9e530b60fe89db5ea05d
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-09 04:02:40 -0300

    Gallery section

---
+++ components/GalleryHome/index.js
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+import data from '../../data/images.json';
+import { GalleryHomeContainer } from './style';
+
+function GalleryHome() {
+    // console.log(data)
+  return (
+    <GalleryHomeContainer>
+        
+        <p className='subtitle'>Galeria</p>
+        <div className='gallery__container'>
+            {data.map ((image) => {
+                    return (
+                        <a
+                            className='image__container'
+                            href={image.url}
+                            rel="noreferrer noopener"
+                            target="_blank"
+                        >
+                            <Image src={image.image} layout="fill" />
+                        </a>
+                        // <img src={image.url} />
+                    )
+                }
+            )}
+        </div>
+        <a  
+            className='viewmore__gallery'
+            href="https://www.instagram.com/hevelenjeronymo/"
+            rel="noreferrer noopener"
+            target="_blank"
+        >
+            View More
+        </a>
+    </GalleryHomeContainer>
+  )
+}
+
+export default GalleryHome
---
+++ components/GalleryHome/style.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const GalleryHomeContainer = styled.section`
+    width: 100%;
+    display: block;
+
+    .gallery__container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        /* -webkit-column-count: 3;
+        -moz-column-count: 3;
+        column-count: 3;
+        -webkit-column-width: 33%;
+        -moz-column-width: 33%;
+        column-width: 33%;
+        padding: 0 12px; */
+
+        .image__container {
+            position: relative;
+            width: 100%;
+            height: 400px;
+            object-fit: cover;
+            object-position: 50% 50%;
+            
+            img {
+                object-fit: cover;
+                transition: all 150ms ease-out;
+            }
+
+            &:hover img {
+                transform: scale(1.1);
+            }
+        }
+
+        @media screen and (max-width: 790px){
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        @media screen and (max-width: 490px){
+            grid-template-columns: repeat(1, 1fr);
+        }
+    }
+
+    .viewmore__gallery {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        background-color: ${({theme}) => theme.secondary};
+        color: ${({theme}) => theme.white};
+        width: 100%;
+        padding: 2rem 0;
+        font-family: 'Teko', sans-serif;
+        font-size: 2.5rem;
+        border: 2px solid transparent;
+        box-shadow: 0px 0px 4px rgba(0,0,0,.8);
+        transition: all 150ms ease-out;
+
+        &:hover {
+            border: 2px solid ${({theme}) => theme.secondary};
+            color: ${({theme}) => theme.secondary};
+            background-color: transparent;
+        }
+    }
+
+    .subtitle {
+        font-size: 1.8rem;
+        font-family: 'Teko', sans-serif;
+        color: ${({theme}) => theme.secondary};
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        display: flex;
+        align-items: center;
+        max-width: 1400px;
+        width: 100%;
+        margin: 0 auto 2rem;
+
+        &::before {
+            content: '';
+            display: block;
+            width: 50px;
+            height: 3px;
+            background-color: ${({theme}) => theme.secondary};
+            margin-right: 1rem;
+        }
+    }
+`;
