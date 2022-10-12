4c12764d55a3e1cd3fd6ff01f93be9d335133c99
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-12 17:07:47 -0300

    Aside section

---
+++ components/Aside/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AsideContainer } from './style';
+
+function Aside({categories}) {
+  return (
+    <AsideContainer>
+        Aside
+    </AsideContainer>
+  )
+}
+
+export default Aside
---
+++ components/Aside/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const AsideContainer = styled.aside`
+    max-width: 400px;
+    width: 100%;
+`;
