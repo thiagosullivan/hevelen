47bfb7d87a5e2ca5ddc3076b8bbb8db49b48b044
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-19 20:23:35 -0300

    About infos

--- components/AboutHome/index.js
+++ components/AboutHome/index.js
@@ -6,16 +6,19 @@ import { AboutHomeContainer } from './style';
 
 function AboutHome() {
   return (
-    <AboutHomeContainer>
+    <AboutHomeContainer id="about">
         <div className='abouthome__col1'>
-            <h2>We are Fitloss. A Dedicated Fitness Center Since 2004</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-            <ButtonJoin text='Join Us' link="/" />
+            <h2>Quem é Hevelen Jeronymo?</h2>
+            <p className='about__p1'>Olá, muito prazer, eu sou a Hevelen Jeronymo, tenho 37 anos, sou apaixonada pelo movimento e entusiasta da saúde. Comecei no esporte ainda criança com aulas de natação, sempre muito ativa e apaixonada por esportes. Durante a adolescência conheci várias modalidades individuais do fitness e aí encontrei minha verdadeira paixão.</p>
+            <p className='about__p2'>Mas, minha trajetória no esporte nem sempre foi fácil. Aos 19 ingressei na faculdade de Educação Física, meu sonho, mas desisti no primeiro ano. Apesar de amar esportes, naquele momento estava bastante desanimada e passei a não acreditar na profissão que havia escolhido. Acabei fazendo uma mudança 360 e ingressei na área administrativa, onde atuei por 16 anos. Um dos fatores que me levaram até o esporte era o medo que sempre tive de engordar, já que minha genética apontava para isso. Aos 25 anos comecei a correr, &#34;afinal, correr emagrece&#34;, ou era o que eu pensava na época. Com o tempo me apaixonei, comecei a me dedicar cada vez mais.</p>
+            <p className='about__p3'>Quando me casei e mudei para Londrina-Pr, eu e meu marido continuamos na corrida, agora com um grupo profissional, que me envolveu cada vez mais com essa modalidade. Comecei a participar de provas, ganhei troféus e estava cada dia mais resistente até que comecei a me lesionar. Não entendia o que estava acontecendo, pois eu tinha a orientação de meu instrutor, profissional de ponta, e fazia corretamente as atividades de aquecimento, condicionamento, etc. Uma das lesões que tive, a mais grave, me levou a investigar as causas e, depois de uma bateria de exames, descobri uma menopausa precoce, aos 32 anos. Neste momento tudo mudou. Devido a lesão eu não conseguia mais correr, não poderia mais engravidar e de repente tudo perdeu o sentido. Mas eu sabia que tinha que fazer algo e passei a investigar alternativas de treinamento.</p>
+            <p className='about__p4'>Então, descobri o treino funcional &#40;kettlebell&#41;, totalmente compatível com minha situação. Me encantei com a força, e me joguei nos levantamentos básicos. Apesar de estar treinando, algo que sempre me realizou, a vida ainda não tinha sentido para mim. Faltava algo. Foi ai que resolvi retomar meu sonho de juventude e, aos 34 anos, voltei estudar educação física, que hoje faz muito mais sentido para mim ao promover a saúde pelo esporte, algo que sempre acreditei. Com o andamento dos meus treinos funcionais e devido a força e às altas cargas, comecei a ficar muito volumosa, algo que passou a me incomodar bastante. Novamente fui procurar me informar e cheguei até a musculação. Pronto, agora tudo passou fazer sentido para mim, as coisas se encaixaram e cada dia mais me vejo apaixonada por essa prática. A musculação me , mantém em movimento &#40;o que amo&#41; e me ajuda a desfrutar de inúmeros benefícios e possibilidades que eu jamais imaginei.</p>
+            <p className='about__p5'>Acredito que cada experiência que adquiri nestes muitos anos de prática esportiva irão me ajudar a entender melhor meus futuros alunos, suas dores e conseguir oferecer à eles, de forma segura e personalizada, a melhor modalidade de treinamento para a conquista da saúde!</p>
+            <div className='abouthome__col2'>
+                <Image src={AboutImage} width={780} height={900} alt="Heleven Jeronymo"/>
+            </div>
         </div>
-        <div className='abouthome__col2'>
-            <Image src={AboutImage} width={780} height={900} />
-        </div>
+        <ButtonJoin text='Fale comigo' link="https://api.whatsapp.com/send?phone=554396386856" />
     </AboutHomeContainer>
   )
 }
--- components/AboutHome/style.js
+++ components/AboutHome/style.js
@@ -4,13 +4,17 @@ export const AboutHomeContainer = styled.section`
     max-width: 1400px;
     width: 100%;
     margin: 6.8rem auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
 
     .abouthome__col1 {
-        max-width: 550px;
-        width: 100%;
+
+        display: grid;
+        grid-template-areas:
+        "title title"
+        "aboutp1 aboutimg"
+        "aboutp2 aboutimg"
+        "aboutp3 aboutimg"
+        "aboutp4 aboutp4"
+        "aboutp5 aboutp5";
 
         h2 {
             font-family: 'Teko', sans-serif;
@@ -19,6 +23,8 @@ export const AboutHomeContainer = styled.section`
             font-weight: 700;
             color: ${({theme}) => theme.primary};
             margin-bottom: 2.5rem;
+            grid-area: title;
+            letter-spacing: 2px;
         }
         p {
             font-family: 'Montserrat', sans-serif;
@@ -26,11 +32,30 @@ export const AboutHomeContainer = styled.section`
             color: ${({theme}) => theme.primary};
             text-align: justify;
             margin-bottom: 1rem;
-            line-height: 25px;
-            
-            &:last-of-type {
-                margin-bottom: 5rem;
-            }
+            line-height: 35px;
         }
+        .about__p1 {
+            grid-area: aboutp1;
+        }
+        .about__p2 {
+            grid-area: aboutp2;
+        }
+        .about__p3 {
+            grid-area: aboutp3;
+        }
+        .about__p4 {
+            grid-area: aboutp4;
+        }
+        .about__p5 {
+            grid-area: aboutp5;
+        }
+        .abouthome__col2 {
+            grid-area: aboutimg;
+            margin-left: 2rem;
+            margin-bottom: 2rem;
+        }
+    }
+    a {
+        margin: 3rem auto 0;
     }
 `
