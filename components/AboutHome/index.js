138f46939cab8f54768fc11d40d40765b88e7f35
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-21 02:26:45 -0300

    Show and hide

--- components/AboutHome/index.js
+++ components/AboutHome/index.js
@@ -1,24 +1,32 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonJoin from '../ButtonJoin';
 import AboutImage from '../../assets/about-img.webp';
 import { AboutHomeContainer } from './style';
 
 function AboutHome() {
+
+  const [ showP, setShowP ] = useState(false);
+
   return (
     <AboutHomeContainer id="about">
         <div className='abouthome__col1'>
             <h2>Quem é Hevelen Jeronymo?</h2>
             <p className='about__p1'>Olá, muito prazer, eu sou a Hevelen Jeronymo, tenho 37 anos, sou apaixonada pelo movimento e entusiasta da saúde. Comecei no esporte ainda criança com aulas de natação, sempre muito ativa e apaixonada por esportes. Durante a adolescência conheci várias modalidades individuais do fitness e aí encontrei minha verdadeira paixão.</p>
             <p className='about__p2'>Mas, minha trajetória no esporte nem sempre foi fácil. Aos 19 ingressei na faculdade de Educação Física, meu sonho, mas desisti no primeiro ano. Apesar de amar esportes, naquele momento estava bastante desanimada e passei a não acreditar na profissão que havia escolhido. Acabei fazendo uma mudança 360 e ingressei na área administrativa, onde atuei por 16 anos. Um dos fatores que me levaram até o esporte era o medo que sempre tive de engordar, já que minha genética apontava para isso. Aos 25 anos comecei a correr, &#34;afinal, correr emagrece&#34;, ou era o que eu pensava na época. Com o tempo me apaixonei, comecei a me dedicar cada vez mais.</p>
-            <p className='about__p3'>Quando me casei e mudei para Londrina-Pr, eu e meu marido continuamos na corrida, agora com um grupo profissional, que me envolveu cada vez mais com essa modalidade. Comecei a participar de provas, ganhei troféus e estava cada dia mais resistente até que comecei a me lesionar. Não entendia o que estava acontecendo, pois eu tinha a orientação de meu instrutor, profissional de ponta, e fazia corretamente as atividades de aquecimento, condicionamento, etc. Uma das lesões que tive, a mais grave, me levou a investigar as causas e, depois de uma bateria de exames, descobri uma menopausa precoce, aos 32 anos. Neste momento tudo mudou. Devido a lesão eu não conseguia mais correr, não poderia mais engravidar e de repente tudo perdeu o sentido. Mas eu sabia que tinha que fazer algo e passei a investigar alternativas de treinamento.</p>
-            <p className='about__p4'>Então, descobri o treino funcional &#40;kettlebell&#41;, totalmente compatível com minha situação. Me encantei com a força, e me joguei nos levantamentos básicos. Apesar de estar treinando, algo que sempre me realizou, a vida ainda não tinha sentido para mim. Faltava algo. Foi ai que resolvi retomar meu sonho de juventude e, aos 34 anos, voltei estudar educação física, que hoje faz muito mais sentido para mim ao promover a saúde pelo esporte, algo que sempre acreditei. Com o andamento dos meus treinos funcionais e devido a força e às altas cargas, comecei a ficar muito volumosa, algo que passou a me incomodar bastante. Novamente fui procurar me informar e cheguei até a musculação. Pronto, agora tudo passou fazer sentido para mim, as coisas se encaixaram e cada dia mais me vejo apaixonada por essa prática. A musculação me , mantém em movimento &#40;o que amo&#41; e me ajuda a desfrutar de inúmeros benefícios e possibilidades que eu jamais imaginei.</p>
-            <p className='about__p5'>Acredito que cada experiência que adquiri nestes muitos anos de prática esportiva irão me ajudar a entender melhor meus futuros alunos, suas dores e conseguir oferecer à eles, de forma segura e personalizada, a melhor modalidade de treinamento para a conquista da saúde!</p>
+            <p className='about__p3'>Quando me casei e mudei para Londrina-Pr, eu e meu marido continuamos na corrida, agora com um grupo profissional, que me envolveu cada vez mais com essa modalidade. Comecei a participar de provas, ganhei troféus e estava cada dia mais resistente até que comecei a me lesionar. Não entendia o que estava acontecendo, pois eu tinha a orientação de meu instrutor, profissional de ponta, e fazia corretamente as atividades de aquecimento, condicionamento, etc. Uma das lesões que tive, a mais grave, me levou a investigar as causas e, depois de uma bateria de exames, descobri uma menopausa precoce, aos 32 anos. Neste momento tudo mudou. </p>
+            <button className={`about_showMore ${showP ? "hideBtn" : ''}`} onClick={() => setShowP(true)}>Mostrar mais</button>
+            <div className={`about__pBottom ${showP ? "showAboutP" : ''}`}>
+              <p className='about__p4'>Devido a lesão eu não conseguia mais correr, não poderia mais engravidar e de repente tudo perdeu o sentido. Mas eu sabia que tinha que fazer algo e passei a investigar alternativas de treinamento. Então, descobri o treino funcional &#40;kettlebell&#41;, totalmente compatível com minha situação. Me encantei com a força, e me joguei nos levantamentos básicos. Apesar de estar treinando, algo que sempre me realizou, a vida ainda não tinha sentido para mim. Faltava algo.</p>
+              <p className='about__p5'>Foi ai que resolvi retomar meu sonho de juventude e, aos 34 anos, voltei estudar educação física, que hoje faz muito mais sentido para mim ao promover a saúde pelo esporte, algo que sempre acreditei. Com o andamento dos meus treinos funcionais e devido a força e às altas cargas, comecei a ficar muito volumosa, algo que passou a me incomodar bastante. Novamente fui procurar me informar e cheguei até a musculação. Pronto, agora tudo passou fazer sentido para mim, as coisas se encaixaram e cada dia mais me vejo apaixonada por essa prática. A musculação me , mantém em movimento &#40;o que amo&#41; e me ajuda a desfrutar de inúmeros benefícios e possibilidades que eu jamais imaginei.</p>
+              <p className='about__p6'>Acredito que cada experiência que adquiri nestes muitos anos de prática esportiva irão me ajudar a entender melhor meus futuros alunos, suas dores e conseguir oferecer à eles, de forma segura e personalizada, a melhor modalidade de treinamento para a conquista da saúde!</p>
+              <button className='about_showMore' onClick={() => setShowP(false)}>Mostrar menos</button>
+            </div>
             <div className='abouthome__col2'>
                 <Image src={AboutImage} width={780} height={900} alt="Heleven Jeronymo"/>
             </div>
         </div>
-        <ButtonJoin text='Fale comigo' link="https://api.whatsapp.com/send?phone=554396386856" />
+        {/* <ButtonJoin text='Fale comigo' link="https://api.whatsapp.com/send?phone=554396386856" /> */}
     </AboutHomeContainer>
   )
 }
--- components/AboutHome/style.js
+++ components/AboutHome/style.js
@@ -4,6 +4,7 @@ export const AboutHomeContainer = styled.section`
     max-width: 1400px;
     width: 100%;
     margin: 6.8rem auto;
+    padding: 0 2rem;
 
     .abouthome__col1 {
 
@@ -13,8 +14,8 @@ export const AboutHomeContainer = styled.section`
         "aboutp1 aboutimg"
         "aboutp2 aboutimg"
         "aboutp3 aboutimg"
-        "aboutp4 aboutp4"
-        "aboutp5 aboutp5";
+        "showMore showMore"
+        "aboutpbottom aboutpbottom";
 
         h2 {
             font-family: 'Teko', sans-serif;
@@ -43,16 +44,92 @@ export const AboutHomeContainer = styled.section`
         .about__p3 {
             grid-area: aboutp3;
         }
-        .about__p4 {
-            grid-area: aboutp4;
+        .hideBtn {
+            height: 0 !important;
+            opacity: 0;
+            transition: all 300ms ease-in;
+            overflow-y: hidden;
         }
-        .about__p5 {
-            grid-area: aboutp5;
+        .about__pBottom {
+            grid-area: aboutpbottom;
+            /* transform: rotateX(-90deg); */
+            height: 0;
+            opacity: 0;
+            transition: all 300ms ease-in;
+            overflow-y: hidden;
+        }
+        .showAboutP {
+            /* transform: rotateX(0deg); */
+            opacity: 1;
+            height: 105%;
+            margin-top: -30px;
         }
         .abouthome__col2 {
             grid-area: aboutimg;
             margin-left: 2rem;
             margin-bottom: 2rem;
+        }
+        .about_showMore {
+            grid-area: showMore;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 165px;
+            height: 50px;
+            background-color: ${({theme}) => theme.white};
+            color: ${({theme}) => theme.secondary};
+            text-transform: uppercase;
+            border: 1px solid ${({theme}) => theme.secondary};
+            transition: all 150ms ease-in;
+            margin: 1rem auto;
+
+            &:hover {
+                border: 1px solid ${({theme}) => theme.secondary};
+                background-color: transparent;
+                color: ${({theme}) => theme.secondary};
+                transform: scale(1.02);
+            }
+        }
+        
+        @media screen and (max-width: 1260px) {
+            p {
+                line-height: 25px;
+            }
+        }
+
+        @media screen and (max-width: 1150px){
+            grid-template-areas:
+            "title title title"
+            "aboutp1 aboutimg aboutimg"
+            "aboutp2 aboutimg aboutimg"
+            "aboutp3 aboutp3 aboutp3"
+            "showMore showMore showMore"
+            "aboutpbottom aboutpbottom aboutpbottom";
+
+            p {
+                line-height: 35px;
+            }
+        }
+
+        @media screen and (max-width: 1180px) {
+            p {
+                line-height: 25px;
+            }
+        }
+
+        @media screen and (max-width: 960px) {
+            grid-template-areas:
+            "title"
+            "aboutimg"
+            "aboutp1"
+            "aboutp2"
+            "aboutp3"
+            "showMore"
+            "aboutpbottom";
+
+            h2 {
+                text-align: center;
+            }
         }
     }
     a {
